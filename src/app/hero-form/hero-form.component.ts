b1bd6a0afaaf3d101877e45821e97efb70996f84
Pereverziev Andrii <pereverziev.andrii@gmail.com>
2020-02-14 17:54:47 +0200

    Finish template-driven fomrs

--- src/app/hero-form/hero-form.component.ts
+++ src/app/hero-form/hero-form.component.ts
@@ -1,18 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { Hero }    from '../hero';
-
+import { Component } from '@angular/core';
+import { Hero } from '../hero';
 
 @Component({
   selector: 'app-hero-form',
   templateUrl: './hero-form.component.html',
   styleUrls: ['./hero-form.component.scss']
 })
-export class HeroFormComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class HeroFormComponent {
 
   powers = ['Really Smart', 'Super Flexible',
             'Super Hot', 'Weather Changer'];
@@ -22,6 +16,10 @@ export class HeroFormComponent implements OnInit {
   submitted = false;
 
   onSubmit() { this.submitted = true; }
+
+  newHero() {
+    this.model = new Hero(42, '', '');
+  }
 
   // TODO: Remove this when we're done
   get diagnostic() { return JSON.stringify(this.model); }
